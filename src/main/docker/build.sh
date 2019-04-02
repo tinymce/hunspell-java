07bf177e6ad4df1996195f4f4cb868f9d3b8e2ee
Ole Krüger <okrueger@cognesys.de>
2019-04-02 15:20:21 +0200

    Fix build script

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -2,14 +2,14 @@
 set -o errexit nounset xtrace pipefail
 autoreconf -vfi
 
-make clean
 ./configure --disable-static --host=x86_64-w64-mingw32
+make clean
 make
 cp ./src/hunspell/.libs/libhunspell-*.dll ../../resources/org/bridj/lib/win64/libhunspell.dll
 strip ../../resources/org/bridj/lib/win64/libhunspell.dll
 
-make clean
 ./configure --disable-static
+make clean
 make
 cp ./src/hunspell/.libs/libhunspell-*.so ../../resources/org/bridj/lib/linux_x64/libhunspell.so
 strip ../../resources/org/bridj/lib/linux_x64/libhunspell.so
