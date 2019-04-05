731a1ea17c3c14d9948b80e27ce22788f62b296b
Ole Krüger <okrueger@cognesys.de>
2019-04-05 13:01:37 +0200

    Fix library name for windows

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -5,8 +5,8 @@ autoreconf -vfi
 ./configure --disable-static --host=x86_64-w64-mingw32
 make clean
 make
-cp ./src/hunspell/.libs/libhunspell-*.dll ../../resources/org/bridj/lib/win64/libhunspell.dll
-strip ../../resources/org/bridj/lib/win64/libhunspell.dll
+cp ./src/hunspell/.libs/libhunspell-*.dll ../../resources/org/bridj/lib/win64/hunspell.dll
+strip ../../resources/org/bridj/lib/win64/hunspell.dll
 
 ./configure --disable-static
 make clean
